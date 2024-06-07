7968cbedb7cfbcaf01104798664db4cbecb3c4d2
AbojawadH <abojawad0099@gmail.com>
2024-06-07 13:44:07 +0300

    someChanges

--- lib/VIEW/CardForDetilseEcommersAndResturant.dart
+++ lib/VIEW/CardForDetilseEcommersAndResturant.dart
@@ -35,110 +35,129 @@ class CardForDetilseEcommersAndResturant extends StatelessWidget {
     return InkWell(
       onTap: () {},
       child: KH_ShadowCard(
-          innerPadding: 10,
-          innerPaddingvertical: 10,
-          height: 120,
-          width: double.infinity,
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset(
-                  image,
-                  height: 110,
-                  width: 95,
-                  fit: BoxFit.cover,
+        innerPadding: 10,
+        innerPaddingvertical: 10,
+        height: 120,
+        width: double.infinity,
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                image,
+                height: 110,
+                width: 95,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      nameresturantorecommers,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      contentRestOrEcomm,
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                    const Divider(
+                      height: 10,
+                      color: Colors.black,
+                    ),
+                      const SizedBox(
+                      height: 2,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: KH_SplashedCard(
+                            borderRadius: 8,
+                            height: 30,
+                            bgColor: Colors.orange[50],
+                            outsideMargin: 5,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                ),
+                                const SizedBox(
+                                  width: 2,
+                                ),
+                                Text(evaluationNumber)
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: KH_SplashedCard(
+                            borderRadius: 8,
+                            bgColor: Colors.blue[100],
+                            outsideMargin: 5,
+                            height: 30,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.delivery_dining_outlined,
+                                  color: Colors.red,
+                                ),
+                                const SizedBox(
+                                  width: 2,
+                                ),
+                                Text(pricedelivry)
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: KH_SplashedCard(
+                            borderRadius: 8,
+                            bgColor: Colors.green[100],
+                            outsideMargin: 5,
+                            height: 30,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 25,
+                                  width: 40,
+                                  child: IconButton(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 10),
+                                    icon: const Icon(Icons.offline_pin_rounded),
+                                    onPressed: () {},
+                                    color: Colors.green,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 2,
+                                ),
+                                Text(openOrClosed)
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
               ),
-              const SizedBox(
-                width: 10,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    nameresturantorecommers,
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    contentRestOrEcomm,
-                    style: const TextStyle(fontSize: 14),
-                  ),
-                  const Divider(
-                    height: 10,
-                    color: Colors.black,
-                  ),
-                  Row(
-                    children: [
-                      KH_SplashedCard(
-                          borderRadius: 8,
-                          height: 30,
-                          bgColor: Colors.orange[50],
-                          outsideMargin: 5,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              const Icon(
-                                Icons.star,
-                                color: Colors.yellow,
-                              ),
-                              const SizedBox(
-                                width: 5,
-                              ),
-                              Text(evaluationNumber)
-                            ],
-                          )),
-                      KH_SplashedCard(
-                          borderRadius: 8,
-                          bgColor: Colors.blue[100],
-                          outsideMargin: 5,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              const Icon(
-                                Icons.delivery_dining_outlined,
-                                color: Colors.red,
-                              ),
-                              const SizedBox(
-                                width: 5,
-                              ),
-                              Text(pricedelivry)
-                            ],
-                          )),
-                      KH_SplashedCard(
-                          borderRadius: 8,
-                          bgColor: Colors.green[100],
-                          outsideMargin: 5,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Container(
-                                height: 25,
-                                width: 40,
-                                child: IconButton(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10),
-                                  icon: const Icon(Icons.offline_pin_rounded),
-                                  onPressed: () {},
-                                  color: Colors.green,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 5,
-                              ),
-                              Text(openOrClosed)
-                            ],
-                          )),
-                    ],
-                  )
-                ],
-              ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
