a42302102c7832faa07484bf78b7fcab900d4c35
AbojawadH <abojawad0099@gmail.com>
2024-06-07 18:35:13 +0300

    ffff

--- lib/SECREEN/App/HOME TAP/resturant.dart
+++ lib/SECREEN/App/HOME TAP/resturant.dart
@@ -54,77 +54,79 @@ class _StateRestutant extends State<Restutant> {
               style: TextStyle(
                   color: Colors.pinkAccent, fontWeight: FontWeight.bold))),
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Column(
-          children: [
-            //@@@@@@@@//@@@@@@@@@//
-            //@@@@@@@@//@@@@@@@@@// CHOISE CHIP RESTURANT
-            //@@@@@@@@//@@@@@@@@@//
-            Container(
-                height: 50,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: name.length,
-                    itemBuilder: (context, index) {
-                      return Wrap(spacing: 8, runSpacing: 8, children: [
-                        const SizedBox(width: 3),
-                        ChoiceChip(
-                            showCheckmark: false,
-                            selectedColor: Colors.pink[50],
-                            selected: defaultchioseindex == index,
-                            onSelected: (nowSelcted) {
-                              setState(() {
-                                defaultchioseindex = nowSelcted ? index : null;
-                              });
-                            },
-                            label: Text("${name[index]}",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 16,
-                                    color: selected == true
-                                        ? Colors.pink
-                                        : Colors.black)))
-                      ]);
-                    })),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              //@@@@@@@@//@@@@@@@@@//
+              //@@@@@@@@//@@@@@@@@@// CHOISE CHIP RESTURANT
+              //@@@@@@@@//@@@@@@@@@//
+              Container(
+                  height: 50,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: name.length,
+                      itemBuilder: (context, index) {
+                        return Wrap(spacing: 8, runSpacing: 8, children: [
+                          const SizedBox(width: 3),
+                          ChoiceChip(
+                              showCheckmark: false,
+                              selectedColor: Colors.pink[50],
+                              selected: defaultchioseindex == index,
+                              onSelected: (nowSelcted) {
+                                setState(() {
+                                  defaultchioseindex =
+                                      nowSelcted ? index : null;
+                                });
+                              },
+                              label: Text("${name[index]}",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16,
+                                      color: selected == true
+                                          ? Colors.pink
+                                          : Colors.black)))
+                        ]);
+                      })),
 
-            //=======================
+              //=======================
 
-            //@@@@@@@@//@@@@@@@@@//
-            //@@@@@@@@//@@@@@@@@@// TEXT FORM FILED (SEARCH) RESTURANT
-            //@@@@@@@@//@@@@@@@@@//
-            KH_FormTextField(
-                padding: const EdgeInsets.all(10),
-                placeHolder: "ابحث",
-                leadingInnerPrefixIconOrWidget:
-                    const Icon(Icons.delete_outline),
-                trailingInnerPrefixIconOrWidget: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.search,
-                      size: 20,
-                    ))),
+              //@@@@@@@@//@@@@@@@@@//
+              //@@@@@@@@//@@@@@@@@@// TEXT FORM FILED (SEARCH) RESTURANT
+              //@@@@@@@@//@@@@@@@@@//
+              KH_FormTextField(
+                  padding: const EdgeInsets.all(10),
+                  placeHolder: "ابحث",
+                  leadingInnerPrefixIconOrWidget:
+                      const Icon(Icons.delete_outline),
+                  trailingInnerPrefixIconOrWidget: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                        size: 20,
+                      ))),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            //@@@@@@@@//@@@@@@@@@//
-            //@@@@@@@@//@@@@@@@@@// AdvetismentsSliderCourser
-            //@@@@@@@@//@@@@@@@@@//
-            const AdvetismentsSliderCourser(),
-            const SizedBox(height: 20),
+              //@@@@@@@@//@@@@@@@@@//
+              //@@@@@@@@//@@@@@@@@@// AdvetismentsSliderCourser
+              //@@@@@@@@//@@@@@@@@@//
+              const AdvetismentsSliderCourser(),
+              const SizedBox(height: 20),
 
-            //@@@@@@@@//@@@@@@@@@//
-            //@@@@@@@@//@@@@@@@@@//CardForDetilseEcommersAndResturant
-            //@@@@@@@@//@@@@@@@@@//
-            const CardForDetilseEcommersAndResturant(
-                nameresturantorecommers: "القبطان",
-                contentRestOrEcomm: "ِشاورما , كربسيي",
-                openOrClosed: "open",
-                image: ImageAsset.eat,
-                evaluationNumber: " 4.99",
-                pricedelivry: "100")
-          ],
+              //@@@@@@@@//@@@@@@@@@//
+              //@@@@@@@@//@@@@@@@@@//CardForDetilseEcommersAndResturant
+              //@@@@@@@@//@@@@@@@@@//
+              const CardForDetilseEcommersAndResturant(
+                  nameresturantorecommers: "القبطان",
+                  contentRestOrEcomm: "ِشاورما , كربسيي",
+                  openOrClosed: "open",
+                  image: ImageAsset.eat,
+                  evaluationNumber: " 4.99",
+                  pricedelivry: "100")
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
